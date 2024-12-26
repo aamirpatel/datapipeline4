951f917b74e0645de3db1fcc54a29edb8d0fe704
Aamir Patel <paamir89@gmail.com>
2024-12-26 18:27:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "data_bucket" {
   bucket = "my-data-pipeline-bucket"
-  acl    = "private"
+  aws_s3_bucket_acl    = "private"
 }
 
 resource "aws_iam_role" "lambda_execution_role" {
@@ -24,7 +24,7 @@ resource "aws_iam_role" "lambda_execution_role" {
 
 resource "aws_lambda_function" "data_processor" {
   function_name = "DataProcessor"
-  role          = aws_iam_role.lambda_execution_role.arn
+  role          = arn:aws:iam::985539789378:role/AWSGlueServiceRole
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
   
@@ -45,7 +45,7 @@ resource "aws_glue_catalog_database" "data_catalog" {
 
 resource "aws_glue_job" "data_transform_job" {
   name     = "data-transform-job"
-  role     = aws_iam_role.lambda_execution_role.arn
+  role     = arn:aws:iam::985539789378:role/AWSGlueServiceRole
   command {
     name            = "glueetl"
     script_location = "s3://my-scripts-bucket/my-script.py"
@@ -53,7 +53,7 @@ resource "aws_glue_job" "data_transform_job" {
   max_capacity = 10
 }
 
-resource "aws_s3_bucket_object" "lambda_script" {
+resource "aws_s3_object" "lambda_script" {
   bucket = aws_s3_bucket.data_bucket.bucket
   key    = "scripts/my_script.py"
   source = "scripts/my_script.py"  # Local path to your Glue script
