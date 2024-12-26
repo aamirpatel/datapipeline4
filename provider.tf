406072cb20ab3b71bbcd290d7da66ea91f76b8c5
Aamir Patel <paamir89@gmail.com>
2024-12-26 18:38:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,18 @@ provider "aws" {
 
 resource "aws_s3_bucket" "data_bucket" {
   bucket = "my-data-pipeline-bucket"
-  aws_s3_bucket_acl    = "private"
+}
+resource "aws_s3_bucket_ownership_controls" "data_bucket" {
+  bucket = aws_s3_bucket.data_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+resource "aws_s3_bucket_acl" "data_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.data_bucket]
+
+  bucket = aws_s3_bucket.data_bucket.id
+  acl    = "private"
 }
 
 resource "aws_iam_role" "lambda_execution_role" {
