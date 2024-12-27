aa7c31ba80f40b151b45b1e9a072b00ea69c52cc
Aamir Patel <paamir89@gmail.com>
2024-12-27 11:16:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -52,7 +52,7 @@ resource "aws_iam_role" "lambda_execution_role" {
 
 resource "aws_lambda_function" "data_processor" {
   function_name = "DataProcessor"
-  role          = "arn:aws:iam::985539789378:role/AWSGlueServiceRole"
+  role          = "arn:aws:iam::985539789378:role/lambda-execution-role"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
   
